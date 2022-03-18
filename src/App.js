915d9e4c8a54f1451fcdb3b761f5e5ed545e2318
Ismat27 <46388260+Ismat27@users.noreply.github.com>
2022-03-18 17:07:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import './App.css'
+import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 4e5416e9
+
+const API_URL = 'http://www.omdbapi.com/?apikey=4e5416e9';
+const movie1 = {
+    "Title": "Underworld: Evolution",
+    "Year": "2006",
+    "imdbID": "tt0401855",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjEzNDY1OTQwOV5BMl5BanBnXkFtZTcwNjcxMTIzMw@@._V1_SX300.jpg"
 }
 
-export default App;
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("")
+    const [message, setMessage] = useState("Search Movies")
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        // getting the movies only with data.Search
+        setMovies(data.Search); 
+        if (movies.length == 0) {
+            setMessage('no movies found')
+        }
+    }
+
+    // useEffect(() => {
+    //     searchMovies('')
+    // }, []);
+
+    return (
+        <div className="app">
+            <h1>MovieArena</h1>
+
+            <div className="search">
+                <input
+                    placeholder="search for movies"
+                    value={searchTerm}
+                    onChange = {(e) => setSearchTerm(e.target.value)}
+                 />
+
+                 <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                  />
+            </div>
+
+            {
+                movies?.length > 0 ?
+                (<div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie} />
+                    ))}
+                </div>): 
+
+                (<div className="empty">
+                    <h2>{message}</h2>
+                </div>)
+            }
+            
+        </div>
+    );
+}
+
+export default App
